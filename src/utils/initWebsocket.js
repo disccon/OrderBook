8dd89bec3e045e9b126e1c78eb7987f21ea5a20a
Modis <dissccon@gmail.com>
2019-12-04 10:54:12 +0200

    change initWebsocket

--- src/utils/initWebsocket.js
+++ src/utils/initWebsocket.js
@@ -1,21 +1,13 @@
 import { eventChannel } from 'redux-saga'
-import _ from 'lodash/core'
 
 
 export function initWebsocket({ url }) {
   return eventChannel(emitter => {
     const ws = new WebSocket(url)
-    let prevData = {}
     ws.onmessage = e => {
       try {
         const data = JSON.parse(e.data)
-        if (!_.isEqual(data.bids, prevData.bids) && !_.isEqual(data.asks, prevData.asks)) {
-          emitter({ type: 'FETCH_ORDER__SUCCESS', data, close: () => ws.close() })
-          prevData = { ...data }
-        } else {
-          emitter({ type: 'FETCH_ORDER__NOT_CHANGED', close: () => ws.close() })
-          prevData = { ...data }
-        }
+        emitter({ type: 'FETCH_ORDER__SUCCESS', data, close: () => ws.close() })
       } catch (error) {
         emitter({ type: 'FETCH_ORDER__FAILURE', error })
         ws.close()
